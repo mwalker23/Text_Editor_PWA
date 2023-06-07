304641284715dee695e6089d5153ee2ecaa42fe3
Mona Lisa <madison0327@hotmail.com>
2023-06-07 04:36:55 -0400

    added to database using the idb module

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,26 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  console.error('putDb not implemented');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+};
+// logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.error('getDb not implemented');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  
+  // Check if a variable is defined and if it is, return it
+  return result?.value;
+};
 
 initdb();
